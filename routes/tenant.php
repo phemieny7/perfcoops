7ffe330fa99c23602cc7feb7296d1a3f29743fad
phemieny7 <oyewo.oluwafemi@gmail.com>
2021-01-10 16:25:47 +0800

    'multitenancy'

--- routes/tenant.php
+++ routes/tenant.php
@@ -24,6 +24,7 @@ Route::middleware([
     PreventAccessFromCentralDomains::class,
 ])->group(function () {
     Route::get('/', function () {
+        // dd(\App\User::all());
         return 'This is your multi-tenant application. The id of the current tenant is ' . tenant('id');
     });
 });
